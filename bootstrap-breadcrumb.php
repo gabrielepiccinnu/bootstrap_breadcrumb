edd3449ec460654143d69b421483e70268ee32fd
Gabriele Piccinnu <gabrielepiccinnu@gmail.com>
2021-10-29 11:25:29 +0200

    Update bootstrap-breadcrumb.php
    Fixed conditionals if the current page is Home, Static Homepage, Blog Page
    http://codex.wordpress.org/Conditional_Tags#The_Blog_Page

--- bootstrap-breadcrumb.php
+++ bootstrap-breadcrumb.php
@@ -1,13 +1,14 @@
 <?php
+
 /**
-* 
-* @version 1.0
-*
-* @author ajulien
-* @link https://github.com/ajulien-fr/bootstrap_breadcrumb/blob/master/bootstrap-breadcrumb.php
-* 
-* I just update for schema.org compatibility 
-*/
+ * 
+ * @version 1.0
+ *
+ * @author ajulien
+ * @link https://github.com/ajulien-fr/bootstrap_breadcrumb/blob/master/bootstrap-breadcrumb.php
+ * 
+ * I just update for schema.org compatibility 
+ */
 
 
 /**
@@ -16,24 +17,25 @@
  * @param  array $visited Optional. Already linked to categories to prevent duplicates.
  * @return string|WP_Error A list of category parents on success, WP_Error on failure.
  */
-function custom_get_category_parents( $id, $visited = array(), $taxonomy = 'category' ) {
+function custom_get_category_parents($id, $visited = array(), $taxonomy = 'category')
+{
 
     $chain = '';
     $sep = '<i class="las la-angle-right"></i>'; // (Line Awesome Font)
     $position = 1;
-    $parent = get_term( $id, $taxonomy );
+    $parent = get_term($id, $taxonomy);
 
-    if ( is_wp_error( $parent ) )
+    if (is_wp_error($parent))
         return $parent;
 
     $name = $parent->name;
 
-    if ( $parent->parent && ( $parent->parent != $parent->term_id ) && !in_array( $parent->parent, $visited ) ) {
+    if ($parent->parent && ($parent->parent != $parent->term_id) && !in_array($parent->parent, $visited)) {
         $visited[] = $parent->parent;
-        $chain .= custom_get_category_parents( $parent->parent, $visited );
+        $chain .= custom_get_category_parents($parent->parent, $visited);
     }
 
-    $chain .= '<li itemscope itemtype="https://schema.org/ListItem" class="breadcrumb-item"><a href="' . esc_url( get_category_link( $parent->term_id ) ) . '" itemprop="item">' . $sep . '<span itemprop="name">' . $name . '</span></a>' .  '</span><meta itemprop="position" content="' . $position++ .'" /></li>';
+    $chain .= '<li itemscope itemtype="https://schema.org/ListItem" class="breadcrumb-item"><a href="' . esc_url(get_category_link($parent->term_id)) . '" itemprop="item">' . $sep . '<span itemprop="name">' . $name . '</span></a>' .  '</span><meta itemprop="position" content="' . $position++ . '" /></li>';
 
     return $chain;
 }
@@ -42,7 +44,8 @@ function custom_get_category_parents( $id, $visited = array(), $taxonomy = 'cate
  * Breadcrumb Function]
  * @return [string] [breadcrumb output]
  */
-function bootstrap_breadcrumb() {
+function bootstrap_breadcrumb()
+{
     global $post;
     //$schema_link = 'http://data-vocabulary.org/Breadcrumb'; (discontinued by Google in favor of schema.org)
     $schema_link = 'https://schema.org/ListItem';
@@ -51,136 +54,125 @@ function bootstrap_breadcrumb() {
     $html = '<ol class="breadcrumb" itemscope itemtype="https://schema.org/BreadcrumbList">';
     $sep = '<i class="las la-angle-right"></i>'; // (Line Awesome Font)
     $position = 1;
-    if ( (is_front_page()) || (is_home()) ) {
-        $html .= '<li itemprop="itemListElement" itemscope itemtype="' . $schema_link . '" class="breadcrumb-item active" aria-current="page"><span itemprop="name">' . __('Home', 'wordflex') .  '</span><meta itemprop="position" content="' . $position++ .'" /></li>';
-    }
+    if ((is_front_page()) && (is_home())) {
+        $html .= '<li itemprop="itemListElement" itemscope itemtype="' . $schema_link . '" class="breadcrumb-item active" aria-current="page"><span itemprop="name">' . __('Home', 'gabrielepiccinnu') .  '</span><meta itemprop="position" content="' . $position++ . '" /></li>';
+    } elseif (is_front_page()) {
+        // static homepage
+        $html .= '<li itemscope itemtype="' . $schema_link . '" class="breadcrumb-item active" aria-current="page"><span itemprop="title">' . __('Home', 'gabrielepiccinnu') . '</span></li>';
+    } elseif (is_home()) {
+        // blog page
+        $html .= '<li itemscope itemtype="' . $schema_link . '" class="breadcrumb-item active" aria-current="page"><span itemprop="title">' . __('Blog', 'gabrielepiccinnu') . '</span></li>';
+    } else {
+        $html .= '<li itemprop="itemListElement" itemscope itemtype="' . $schema_link . '" class="breadcrumb-item"><a href="' . esc_url(home_url('/')) . '" itemprop="item"><span itemprop="name">' . __('Home', 'gabrielepiccinnu') . '</span></a><meta itemprop="position" content="' . $position++ . '" /></li>';
 
-    else {
-        $html .= '<li itemprop="itemListElement" itemscope itemtype="' . $schema_link . '" class="breadcrumb-item"><a href="' . esc_url(home_url('/')) . '" itemprop="item"><span itemprop="name">' . __('Home', 'wordflex') . '</span></a><meta itemprop="position" content="' . $position++ .'" /></li>';
-
-        if ( is_attachment() ) {
+        if (is_attachment()) {
             $parent = get_post($post->post_parent);
             $categories = get_the_category($parent->ID);
 
-            if ( $categories[0] ) {
+            if ($categories[0]) {
                 $html .= custom_get_category_parents($categories[0]);
             }
 
-            $html .= '<li itemprop="itemListElement" itemscope itemtype="' . $schema_link . '" class="breadcrumb-item"><a href="' . esc_url( get_permalink( $parent ) ) . '" itemprop="item">' . $sep . '<span itemprop="name">' . $parent->post_title . '</span></a><meta itemprop="position" content="' . $position++ .'" /></li>';
+            $html .= '<li itemprop="itemListElement" itemscope itemtype="' . $schema_link . '" class="breadcrumb-item"><a href="' . esc_url(get_permalink($parent)) . '" itemprop="item">' . $sep . '<span itemprop="name">' . $parent->post_title . '</span></a><meta itemprop="position" content="' . $position++ . '" /></li>';
             $html .= $sep;
-            $html .= '<li itemprop="itemListElement" itemscope itemtype="' . $schema_link . '" class="breadcrumb-item active" aria-current="page">' . $sep . '<span itemprop="name">' . get_the_title() . '</span><meta itemprop="position" content="' . $position++ .'" /></li>';
+            $html .= '<li itemprop="itemListElement" itemscope itemtype="' . $schema_link . '" class="breadcrumb-item active" aria-current="page">' . $sep . '<span itemprop="name">' . get_the_title() . '</span><meta itemprop="position" content="' . $position++ . '" /></li>';
         }
         if (get_page_by_path('blog')) {
             if (!is_page()) {
-                $html .= '<li itemprop="itemListElement" itemscope itemtype="' . $schema_link . '" class="breadcrumb-item"><a href="' . get_permalink(get_page_by_path('blog')) . '" itemprop="item">' . $sep . '<span itemprop="name">' . __('Blog', 'wordflex') . '</span></a><meta itemprop="position" content="' . $position++ .'" /></li>';
+                $html .= '<li itemprop="itemListElement" itemscope itemtype="' . $schema_link . '" class="breadcrumb-item"><a href="' . get_permalink(get_page_by_path('blog')) . '" itemprop="item">' . $sep . '<span itemprop="name">' . __('Blog', 'gabrielepiccinnu') . '</span></a><meta itemprop="position" content="' . $position++ . '" /></li>';
             }
         }
 
-        if ( is_category() ) {
-            $category = get_category( get_query_var( 'cat' ) );
+        if (is_category()) {
+            $category = get_category(get_query_var('cat'));
 
-            if ( $category->parent != 0 ) {
-                $html .= custom_get_category_parents( $category->parent );
+            if ($category->parent != 0) {
+                $html .= custom_get_category_parents($category->parent);
             }
 
-            $html .= '<li itemprop="itemListElement" itemscope itemtype="' . $schema_link . '" class="breadcrumb-item active" aria-current="page">' . $sep . '<span itemprop="name">' . single_cat_title( '', false ) .  '</span><meta itemprop="position" content="' . $position++ .'" /></li>';
-        }
-
-
-        elseif ( is_page() && !is_front_page() ) {
+            $html .= '<li itemprop="itemListElement" itemscope itemtype="' . $schema_link . '" class="breadcrumb-item active" aria-current="page">' . $sep . '<span itemprop="name">' . single_cat_title('', false) .  '</span><meta itemprop="position" content="' . $position++ . '" /></li>';
+        } elseif (is_page() && !is_front_page()) {
             $parent_id = $post->post_parent;
             $parent_pages = array();
 
-            while ( $parent_id ) {
+            while ($parent_id) {
                 $page = get_page($parent_id);
                 $parent_pages[] = $page;
                 $parent_id = $page->post_parent;
             }
 
-            $parent_pages = array_reverse( $parent_pages );
+            $parent_pages = array_reverse($parent_pages);
 
-            if ( !empty( $parent_pages ) ) {
-                foreach ( $parent_pages as $parent ) {
-                    $html .= '<li itemprop="itemListElement" itemscope itemtype="' . $schema_link . '" class="breadcrumb-item"><a href="' . esc_url( get_permalink( $parent->ID ) ) . '" itemprop="item">' . $sep . '<span itemprop="name">' . get_the_title( $parent->ID ) . '</span></a><meta itemprop="position" content="' . $position++ .'" /></li>';
+            if (!empty($parent_pages)) {
+                foreach ($parent_pages as $parent) {
+                    $html .= '<li itemprop="itemListElement" itemscope itemtype="' . $schema_link . '" class="breadcrumb-item"><a href="' . esc_url(get_permalink($parent->ID)) . '" itemprop="item">' . $sep . '<span itemprop="name">' . get_the_title($parent->ID) . '</span></a><meta itemprop="position" content="' . $position++ . '" /></li>';
                 }
             }
 
-            $html .= '<li itemprop="itemListElement" itemscope itemtype="' . $schema_link . '" class="breadcrumb-item active" aria-current="page">' . $sep . '<span itemprop="name">' . get_the_title() .  '</span><meta itemprop="position" content="' . $position++ .'" /></li>';
-        }
-
-        elseif ( is_singular( 'post' ) ) {
+            $html .= '<li itemprop="itemListElement" itemscope itemtype="' . $schema_link . '" class="breadcrumb-item active" aria-current="page">' . $sep . '<span itemprop="name">' . get_the_title() .  '</span><meta itemprop="position" content="' . $position++ . '" /></li>';
+        } elseif (is_singular('post')) {
             $categories = get_the_category();
 
-            if ( $categories[0] ) {
+            if ($categories[0]) {
                 $html .= custom_get_category_parents($categories[0]);
             }
 
-            $html .= '<li itemprop="itemListElement" itemscope itemtype="' . $schema_link . '" class="breadcrumb-item active" aria-current="page">' . $sep . '<span itemprop="name">' . get_the_title() .  '</span><meta itemprop="position" content="' . $position++ .'" /></li>';
-        }
-
-        elseif ( is_tag() ) {
-            $html .= '<li itemprop="itemListElement" itemscope itemtype="' . $schema_link . '" class="breadcrumb-item active" aria-current="page">' . $sep . '<span itemprop="name">' . single_tag_title( '', false ) .  '</span><meta itemprop="position" content="' . $position++ .'" /></li>';
-        }
-
-        elseif ( is_day() ) {
-            $html .= '<li itemprop="itemListElement" itemscope itemtype="' . $schema_link . '" class="breadcrumb-item"><a href="' . esc_url( get_year_link( get_the_time( 'Y' ) ) ) . '" itemprop="item">' . $sep . '<span itemprop="name">' . get_the_time( 'Y' ) . '</span></a><meta itemprop="position" content="' . $position++ .'" /></li>';
-            $html .= '<li itemprop="itemListElement" itemscope itemtype="' . $schema_link . '" class="breadcrumb-item"><a href="' . esc_url( get_month_link( get_the_time( 'Y' ), get_the_time( 'm' ) ) ) . '" itemprop="item">' . $sep . '<span itemprop="name">' . get_the_time( 'm' ) . '</span></a><meta itemprop="position" content="' . $position++ .'" /></li>';
-            $html .= '<li itemprop="itemListElement" itemscope itemtype="' . $schema_link . '" class="breadcrumb-item active" aria-current="page">' . $sep . '<span itemprop="name">' . get_the_time('d') .  '</span><meta itemprop="position" content="' . $position++ .'" /></li>';
-        }
-
-        elseif ( is_month() ) {
-            $html .= '<li itemprop="itemListElement" itemscope itemtype="' . $schema_link . '" class="breadcrumb-item"><a href="' . esc_url( get_year_link( get_the_time( 'Y' ) ) ) . '" itemprop="item">' . get_the_time( 'Y' ) . '</a></li>';
-            $html .= '<li itemprop="itemListElement" itemscope itemtype="' . $schema_link . '" class="breadcrumb-item active" aria-current="page">' . $sep . '<span itemprop="name">' . get_the_time( 'F' ) .  '</span><meta itemprop="position" content="' . $position++ .'" /></li>';
-        }
-
-        elseif ( is_year() ) {
-            $html .= '<li itemprop="itemListElement" itemscope itemtype="' . $schema_link . '" class="breadcrumb-item active" aria-current="page">' . $sep . '<span itemprop="name">' . get_the_time( 'Y' ) .  '</span><meta itemprop="position" content="' . $position++ .'" /></li>';
-        }
-
-        elseif ( is_author() ) {
-            $html .= '<li itemprop="itemListElement" itemscope itemtype="' . $schema_link . '" class="breadcrumb-item active" aria-current="page">' . $sep . '<span itemprop="name">' . get_the_author() .  '</span><meta itemprop="position" content="' . $position++ .'" /></li>';
-        }
-
-        elseif ( is_search() ) {
-
-        }
-
-        elseif ( is_404() ) {
-
-        }
-
-        elseif ( is_singular('service') ) {
+            $html .= '<li itemprop="itemListElement" itemscope itemtype="' . $schema_link . '" class="breadcrumb-item active" aria-current="page">' . $sep . '<span itemprop="name">' . get_the_title() .  '</span><meta itemprop="position" content="' . $position++ . '" /></li>';
+        } elseif (is_tag()) {
+            $html .= '<li itemprop="itemListElement" itemscope itemtype="' . $schema_link . '" class="breadcrumb-item active" aria-current="page">' . $sep . '<span itemprop="name">' . single_tag_title('', false) .  '</span><meta itemprop="position" content="' . $position++ . '" /></li>';
+        } elseif (is_day()) {
+            $html .= '<li itemprop="itemListElement" itemscope itemtype="' . $schema_link . '" class="breadcrumb-item"><a href="' . esc_url(get_year_link(get_the_time('Y'))) . '" itemprop="item">' . $sep . '<span itemprop="name">' . get_the_time('Y') . '</span></a><meta itemprop="position" content="' . $position++ . '" /></li>';
+            $html .= '<li itemprop="itemListElement" itemscope itemtype="' . $schema_link . '" class="breadcrumb-item"><a href="' . esc_url(get_month_link(get_the_time('Y'), get_the_time('m'))) . '" itemprop="item">' . $sep . '<span itemprop="name">' . get_the_time('m') . '</span></a><meta itemprop="position" content="' . $position++ . '" /></li>';
+            $html .= '<li itemprop="itemListElement" itemscope itemtype="' . $schema_link . '" class="breadcrumb-item active" aria-current="page">' . $sep . '<span itemprop="name">' . get_the_time('d') .  '</span><meta itemprop="position" content="' . $position++ . '" /></li>';
+        } elseif (is_month()) {
+            $html .= '<li itemprop="itemListElement" itemscope itemtype="' . $schema_link . '" class="breadcrumb-item"><a href="' . esc_url(get_year_link(get_the_time('Y'))) . '" itemprop="item">' . get_the_time('Y') . '</a></li>';
+            $html .= '<li itemprop="itemListElement" itemscope itemtype="' . $schema_link . '" class="breadcrumb-item active" aria-current="page">' . $sep . '<span itemprop="name">' . get_the_time('F') .  '</span><meta itemprop="position" content="' . $position++ . '" /></li>';
+        } elseif (is_year()) {
+            $html .= '<li itemprop="itemListElement" itemscope itemtype="' . $schema_link . '" class="breadcrumb-item active" aria-current="page">' . $sep . '<span itemprop="name">' . get_the_time('Y') .  '</span><meta itemprop="position" content="' . $position++ . '" /></li>';
+        } elseif (is_author()) {
+            $html .= '<li itemprop="itemListElement" itemscope itemtype="' . $schema_link . '" class="breadcrumb-item active" aria-current="page">' . $sep . '<span itemprop="name">' . get_the_author() .  '</span><meta itemprop="position" content="' . $position++ . '" /></li>';
+        } elseif (is_search()) {
+        } elseif (is_404()) {
+        } elseif (is_singular('service')) {
             //$offercategories = get_the_terms( get_the_ID(), 'property_type' );
- 
+
             /*if ( $offercategories[0] ) {
                 $html .= custom_get_category_parents($offercategories[0], 'property_type');
             }*/
-            
-            $html .= '<li itemprop="itemListElement" itemscope itemtype="' . $schema_link . '" class="breadcrumb-item active" aria-current="page">' . $sep . '<span itemprop="name">' . get_the_title() .  '</span><meta itemprop="position" content="' . $position++ .'" /></li>';
-        }
 
-        elseif ( is_singular('offer') ) {
-            $offercategories = get_the_terms( get_the_ID(), 'offer_category' );
- 
-            if ( $offercategories[0] ) {
+            $html .= '<li itemprop="itemListElement" itemscope itemtype="' . $schema_link . '" class="breadcrumb-item active" aria-current="page">' . $sep . '<span itemprop="name">' . get_the_title() .  '</span><meta itemprop="position" content="' . $position++ . '" /></li>';
+        } elseif (is_singular('offer')) {
+            $offercategories = get_the_terms(get_the_ID(), 'offer_category');
+
+            if ($offercategories[0]) {
                 $html .= custom_get_category_parents($offercategories[0], 'offer_category');
             }
-            
-            $html .= '<li itemprop="itemListElement" itemscope itemtype="' . $schema_link . '" class="breadcrumb-item active" aria-current="page">' . $sep . '<span itemprop="name">' . get_the_title() .  '</span><meta itemprop="position" content="' . $position++ .'" /></li>';
+
+            $html .= '<li itemprop="itemListElement" itemscope itemtype="' . $schema_link . '" class="breadcrumb-item active" aria-current="page">' . $sep . '<span itemprop="name">' . get_the_title() .  '</span><meta itemprop="position" content="' . $position++ . '" /></li>';
+        } elseif (is_singular('property')) {
+            $offercategories = get_the_terms(get_the_ID(), 'property_type');
+
+            if ($offercategories[0]) {
+                $html .= custom_get_category_parents($offercategories[0], 'property_type');
+            }
+
+            $html .= '<li itemprop="itemListElement" itemscope itemtype="' . $schema_link . '" class="breadcrumb-item active" aria-current="page">' . $sep . '<span itemprop="name">' . get_the_title() .  '</span><meta itemprop="position" content="' . $position++ . '" /></li>';
         }
 
-        elseif ( is_singular('property') ) {
-            $offercategories = get_the_terms( get_the_ID(), 'property_type' );
+        /* EXAMPLE: ADD YOUR CPT ON BREADCRUMB STRUCTURE
+          elseif ( is_singular('MY_CPT') ) {
+            $offercategories = get_the_terms( get_the_ID(), 'MY_CPT_TAX' );
  
-            if ( $offercategories[0] ) {
-                $html .= custom_get_category_parents($offercategories[0], 'property_type');
+            if ( $my_cpt_categories[0] ) {
+                $html .= custom_get_category_parents($my_cpt_categories[0], 'MY_CPT_TAX');
             }
             
             $html .= '<li itemprop="itemListElement" itemscope itemtype="' . $schema_link . '" class="breadcrumb-item active" aria-current="page">' . $sep . '<span itemprop="name">' . get_the_title() .  '</span><meta itemprop="position" content="' . $position++ .'" /></li>';
         }
+        */
     }
 
-//  $html .= '</ol></nav>';
+    //  $html .= '</ol></nav>';
     $html .= '</ol>';
 
     echo $html;
